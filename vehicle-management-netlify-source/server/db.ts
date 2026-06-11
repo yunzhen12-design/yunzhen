946465177509d9e210d9786bb663703b66e28db2
yunzhen12-design <yunzhen12@qq.com>
2026-06-11 16:10:26 +0800

    Delete vehicle-management-netlify-source directory

--- vehicle-management-netlify-source/server/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-export const prisma = new PrismaClient()
-
-// Prisma 负责表结构迁移；启动时仅验证数据库连接。
-export async function initSchema() {
-  await prisma.$connect()
-}
